4fd5284e1871d88400ffbda778bf3e31a86b0705
amirz132 <amirzawad@gmail.com>
2022-01-13 14:43:35 -0500

    implemented front-end

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Tilt from 'react-tilt'
+import vision from './vision.png';
+import './Logo.css';
+
+const Logo = () => {
+    return (
+        <div className='ma4 mt0'>
+            <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 250, width: 250 }} >
+                <div className="Tilt-inner pa3"><img style={{paddingTop: '5px'}} alt = 'logo' src={vision}></img></div>
+            </Tilt>
+        </div>
+    );
+}
+
+export default Logo;
